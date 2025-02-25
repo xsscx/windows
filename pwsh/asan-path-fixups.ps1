a4fab7b16b78a42332e6a945797c90e2ceba8e4f
David Hoyt <xss@xss.cx>
2025-02-25 16:03:03 -0500

    Create asan-path-fixups.ps1

---
+++ pwsh/asan-path-fixups.ps1
@@ -0,0 +1,3 @@
+# devenv updates with version strings for automagic builds
+$env:Path = "C:\test\vcpkg\installed\x64-windows\debug\bin;C:\test\vcpkg\installed\x64-windows\debug\lib;" + $env:Path
+$env:Path = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.43.34808\bin\Hostx64\x64\;" + $env:Path
